f7a6de4ee1ae2ba94bde03405c9b9fa6fe2f1c50
dlworua <leegaegyum@gmail.com>
2025-07-21 21:35:14 +0900

    feat: 안드로이드 권한 및 설정 추가, 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,11 +11,12 @@ plugins {
 android {
     namespace = "com.example.lifemood"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -27,8 +28,8 @@ android {
         applicationId = "com.example.lifemood"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -41,6 +42,11 @@ android {
         }
     }
 }
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
+
 
 flutter {
     source = "../.."
